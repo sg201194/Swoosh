5e35fc78dd93a02a8a03b94ff897692839c8fcc9
sg201194 <shubhamagoyal@ymail.com>
2018-03-12 02:27:23 +0530

    lesson 4

--- app/src/main/java/com/example/shubham/swoosh/WelocomeActivity.kt
+++ app/src/main/java/com/example/shubham/swoosh/WelocomeActivity.kt
@@ -1,12 +1,19 @@
 package com.example.shubham.swoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_welocome.*
 
 class WelocomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welocome)
+
+        getStartedBtn.setOnClickListener{
+            val leagueIntent = Intent(this, LeagueActivity::class.java)
+            startActivity(leagueIntent)
+        }
     }
 }
