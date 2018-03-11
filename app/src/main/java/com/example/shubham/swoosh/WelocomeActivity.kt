d0714295f94c445cc5407d71d251e468cb3db02d
sg201194 <shubhamagoyal@ymail.com>
2018-03-11 13:53:43 +0530

    initial commit

---
+++ app/src/main/java/com/example/shubham/swoosh/WelocomeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.shubham.swoosh
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class WelocomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welocome)
+    }
+}
