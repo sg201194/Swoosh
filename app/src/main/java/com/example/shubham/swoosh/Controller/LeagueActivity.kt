accb44e265d96217140c88bd4d70c2bf02f3e5ea
sg201194 <shubhamagoyal@ymail.com>
2018-03-18 04:23:57 +0530

    lesson-07 part 2

--- app/src/main/java/com/example/shubham/swoosh/Controller/LeagueActivity.kt
+++ app/src/main/java/com/example/shubham/swoosh/Controller/LeagueActivity.kt
@@ -3,6 +3,7 @@ package com.example.shubham.swoosh.Controller
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.View
 import android.widget.Toast
 import com.example.shubham.swoosh.Model.Player
@@ -14,11 +15,22 @@ class LeagueActivity : AppCompatActivity() {
 
     var player = Player("", "")
 
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putParcelable(EXTRA_PLAYER, player)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
     }
 
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if(savedInstanceState != null) {
+            player = savedInstanceState.getParcelable(EXTRA_PLAYER)
+        }
+    }
     fun leagueNextClicked(view: View) {
         if (player.league != "") {
             val skillActivity = Intent(this, SkillActivity::class.java)
